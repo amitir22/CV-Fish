01556ce5a0aa0d436ed85ce1e5732a341692120e
amitir22 <amitir22@gmail.com>
2025-03-16 15:29:24 +0200

    Added Timer class that calculates difference in microseconds

---
+++ timer.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+class Timer:
+    """
+    Microseconds timer
+    """
+    def __init__(self):
+        self.start_time = None
+    
+    def tick(self):
+        self.start_time = datetime.now()
+    
+    def tock(self):
+        """
+        Returns:
+            (tock - tick) in microseconds
+        """
+        end_time = datetime.now()
+        delta = end_time - self.start_time
+
+        total_days = delta.days
+        total_seconds = total_days * 24 * 60 * 60 + delta.seconds
+        total_microseconds = total_seconds * 1_000_000 + delta.microseconds
+
+        return total_microseconds
